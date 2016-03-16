7ec0e7f2989790a9e52c52a3e1ec08199f6e6c94
t-kimura <juventus0901@gmail.com>
2016-03-16 17:54:55 +0900

    sample-2 > index.php

--- index.php
+++ index.php
@@ -1,1 +1,417 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/app.min.css">
+	<style>
+	  .box {
+		  padding: 8px 16px;
+		  text-align: center;
+		  background: #f4f4f4;
+		  border: solid 1px #ccc;
+		  font-size: 14px;
+	  }
+	</style>
+</head>
+<body>
+	<header role="banner" class="c-drawer-fixed-bar c-bar">
+		<div class="c-bar__inner">
+		  <div class="c-grid c-grid--gutters">
+			<div class="c-grid__col c-grid__col--4">
+				<h1>Flanky</h1>
+			</div>
+			<div class="c-grid__col c-grid__col--8">
+				<span class="c-drawer-btn c-mediaquery--sp">
+					<i class="c-drawer-btn__icon"></i>
+				</span>
+				<ul class="c-nav c-nav--right c-mediaquery--pc">
+					<li class="c-nav__item">
+						<a href="#">メニュー1</a>
+					</li>
+					<li class="c-nav__item">
+						<a href="#">メニュー2</a>
+					</li>
+					<li class="c-nav__item">
+						<a href="#">メニュー3</a>
+					</li>
+					<li class="c-nav__item">
+						<a href="#">メニュー4</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	  </div>
+	</header>
+	<div class="c-drawer js-drawer c-mediaquery--sp">
+		<div class="c-drawer-inner">
+			<ul class="c-menu">
+				<li class="c-menu__item">
+					<a href="#">
+						メニュー1
+						<i class="c-menu__item__icon fa fa-angle-right"></i>
+					</a>
+				</li>
+				<li class="c-menu__item">
+					<a href="#">
+						メニュー1
+						<i class="c-menu__item__icon fa fa-angle-right"></i>
+					</a>
+				</li>
+				<li class="c-menu__item">
+					<a href="#">
+						メニュー1
+						<i class="c-menu__item__icon fa fa-angle-right"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="c-hero c-hero--anim" style="margin-top: 56px;">
+		<div class="c-hero__inner">
+			<h2 class="c-hero__inner__title">Hero</h2>
+			<p class="c-hero__inner__text">
+				13歳でFCバルセロナに入団し、17歳でトップチームデビューして以降、7度のリーガ・エスパニョーラ優勝、<br>
+				4度のUEFAチャンピオンズリーグ優勝に貢献。自身も5回の世界最優秀選手賞FIFAバロンドールを受賞し[注 2]、<br>
+				2008-09シーズンから2011-12シーズンまで4季連続のチャンピオンズリーグ得点王を獲得。
+			</p>
+			<div>
+				<a href="#" class="c-btn c-btn-default--ghost c-btn--large">ゴーストボタン</a>
+			</div>
+		</div>
+	</div>
+	<main role="main" style="max-width: 1200px; margin: 0 auto;">
+		<div class="c-container">
+			<ul class="c-inline">
+				<li class="c-inline__cell">
+					<a href="#">Home</a> <i class="fa fa-angle-right"></i>
+				</li>
+				<li class="c-inline__cell">
+					<a href="#">BreadCrumb</a>
+				</li>
+			</ul>
+		</div>
+		<div class="c-container">
+			<div class="c-grid c-grid--gutters">
+				<div class="c-grid__col c-grid__col--9">
+					<section>
+						<h2 class="c-title c-title--primary">Card<span class="c-title__sub-text">カードデザイン</span></h2>
+						<div class="c-grid c-grid--gutters">
+							<div class="c-grid__col c-grid__col--4">
+								<div class="c-card">
+									<p class="c-card__thumb">
+										<img src="img/messi.jpg" width="100%" alt="">
+									</p>
+									<div class="c-card__content">
+										<h2>リオネル・メッシ</h2>
+										<p class="c-card__content__clamp">
+											<a href="#">
+											13歳でFCバルセロナに入団し、17歳でトップチームデビューして以降、7度のリーガ・エスパニョーラ優勝、4度のUEFAチャンピオンズリーグ優勝に貢献。自身も5回の世界最優秀選手賞FIFAバロンドールを受賞し[注 2]、2008-09シーズンから2011-12シーズンまで4季連続のチャンピオンズリーグ得点王を獲得。
+											</a>
+										</p>
+									</div>
+									<div class="c-card__bottom">
+										<div class="c-inline c-inline--justify">
+											<p class="c-inline__cell c-text--secondary">
+												アルゼンチン代表
+											</p>
+											<p class="c-inline__cell c-text--secondary">
+												FCバルセロナ
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-grid__col c-grid__col--4">
+								<div class="c-card">
+									<p class="c-card__thumb">
+										<img src="img/cr7.jpg" width="100%" alt="">
+									</p>
+									<div class="c-card__content">
+										<h2>クリスティアーノ・ロナウド</h2>
+										<p class="c-card__content__clamp">
+											<a href="#">
+											ポルトガル・フンシャル出身のサッカー選手。リーガ・エスパニョーラ・レアル・マドリード所属。ポルトガル代表。ポジションはフォワード。愛称はCR7[1]。メディアからはクリロナとも呼ばれる[4][5]。
+											</a>
+										</p>
+									</div>
+									<div class="c-card__bottom">
+										<div class="c-inline c-inline--justify">
+											<p class="c-inline__cell c-text--secondary">
+												ポルトガル代表
+											</p>
+											<p class="c-inline__cell c-text--secondary">
+												レアルマドリード
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-grid__col c-grid__col--4">
+								<div class="c-card">
+									<p class="c-card__thumb">
+										<img src="img/neymar.jpg" width="100%" alt="">
+									</p>
+									<div class="c-card__content">
+										<h2>ネイマール</h2>
+										<p class="c-card__content__clamp">
+											<a href="#">
+											ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。ブラジル代表。ポジションはフォワード。
+											</a>
+										</p>
+									</div>
+									<div class="c-card__bottom">
+										<div class="c-inline c-inline--justify">
+											<p class="c-inline__cell c-text--secondary">
+												ブラジル代表
+											</p>
+											<p class="c-inline__cell c-text--secondary">
+												FCバルセロナ
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="u-ta-r">
+							<a href="#" class="c-btn c-btn-primary--flat">More</a>
+						</div>
+					</section>
+
+					<section>
+						<h2 class="c-title c-title--primary">List<span class="c-title__sub-text">リストデザイン</span></h2>
+
+						<div class="c-card">
+							<ul class="c-menu">
+								<li class="c-menu__item">
+									<a href="#" class="c-media">
+										<div class="c-media__left">
+											<img src="img/messi.jpg" width="100%" alt="">
+										</div>
+										<div class="c-media__text">
+											<h3>リオネル・メッシ</h3>
+											<p class="c-media__text__clmap">
+												13歳でFCバルセロナに入団し、17歳でトップチームデビューして以降、7度のリーガ・エスパニョーラ優勝、4度のUEFAチャンピオンズリーグ優勝に貢献。自身も5回の世界最優秀選手賞FIFAバロンドールを受賞し[注 2]、2008-09シーズンから2011-12シーズンまで4季連続のチャンピオンズリーグ得点王を獲得。
+											</p>
+											<div class="c-inline">
+												<p class="c-inline__cell c-text--secondary">
+													アルゼンチン代表
+												</p>
+												<p class="c-inline__cell c-text--secondary">
+													FCバルセロナ
+												</p>
+											</div>
+										</div>
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+								<li class="c-menu__item">
+									<a href="#" class="c-media">
+										<div class="c-media__left">
+											<img src="img/cr7.jpg" width="100%" alt="">
+										</div>
+										<div class="c-media__text">
+											<h3>クリスティアーノ・ロナウド</h3>
+											<p class="c-media__text__clmap">
+												ポルトガル・フンシャル出身のサッカー選手。リーガ・エスパニョーラ・レアル・マドリード所属。ポルトガル代表。ポジションはフォワード。愛称はCR7[1]。メディアからはクリロナとも呼ばれる[4][5]。
+											</p>
+											<div class="c-inline">
+												<p class="c-inline__cell c-text--secondary">
+													ポルトガル代表
+												</p>
+												<p class="c-inline__cell c-text--secondary">
+													レアルマドリード
+												</p>
+											</div>
+										</div>
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+								<li class="c-menu__item">
+									<a href="#" class="c-media">
+										<div class="c-media__left">
+											<img src="img/neymar.jpg" width="100%" alt="">
+										</div>
+										<div class="c-media__text">
+											<h3>ネイマール</h3>
+											<p class="c-media__text__clmap">
+												ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。ブラジル代表。ポジションはフォワード。
+											</p>
+											<div class="c-inline">
+												<p class="c-inline__cell c-text--secondary">
+													ブラジル代表
+												</p>
+												<p class="c-inline__cell c-text--secondary">
+													FCバルセロナ
+												</p>
+											</div>
+										</div>
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+								<li class="c-menu__item">
+									<a href="#" class="c-media">
+										<h3>リオネル・メッシ</h3>
+										<p class="c-media__text__clmap">
+											13歳でFCバルセロナに入団し、17歳でトップチームデビューして以降、7度のリーガ・エスパニョーラ優勝、4度のUEFAチャンピオンズリーグ優勝に貢献。自身も5回の世界最優秀選手賞FIFAバロンドールを受賞し[注 2]、2008-09シーズンから2011-12シーズンまで4季連続のチャンピオンズリーグ得点王を獲得。
+										</p>
+										<div class="c-inline">
+											<p class="c-inline__cell c-text--secondary">
+												アルゼンチン代表
+											</p>
+											<p class="c-inline__cell c-text--secondary">
+												FCバルセロナ
+											</p>
+										</div>
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="u-ta-r">
+							<a href="#" class="c-btn c-btn-primary--flat">More</a>
+						</div>
+					</section>
+
+					<section>
+						<div class="c-tab js-tab">
+							<ul class="c-tab__menu">
+							  <li class="c-tab__menu__item is-active">
+								<a href="#" class="c-tab-btn c-tab-btn--sp js-tab-btn" data-tabid="1">
+								  Table Design
+								</a>
+							  </li>
+							  <li class="c-tab__menu__item">
+								<a href="#" class="c-tab-btn c-tab-btn--basic js-tab-btn" data-tabid="2">
+								  Form Design
+								</a>
+							  </li>
+							</ul>
+
+							<div class="c-tab__body js-tab-body" data-contentid="1">
+								<div class="c-card">
+								  <table class="c-table">
+										<tr>
+											<th>#</th>
+											<th>First Name</th>
+											<th>Last Name</th>
+											<th>Username</th>
+										</tr>
+										<tr>
+											<th>1</th>
+											<td>hoghoe</td>
+											<td>fugafuga</td>
+											<td>bakabka</td>
+										</tr>
+										<tr>
+											<th>2</th>
+											<td>hoghoe</td>
+											<td>fugafuga</td>
+											<td>bakabka</td>
+										</tr>
+									</table>
+								</div>
+							</div>
+							<div class="c-tab__body js-tab-body is-hidden" data-contentid="2">
+								<div class="c-card">
+								  <form class="c-form">
+										<table class="c-form__table">
+											<tr>
+												<th>テキスト</th>
+												<td><input type="text"></td>
+											</tr>
+											<tr>
+												<th>チェックボックス</th>
+												<td>
+													<input type="checkbox" name="checkbox" id="checkbox1"><label for="checkbox1">checkbox1</label>
+													<input type="checkbox" name="checkbox" id="checkbox2"><label for="checkbox2">checkbox2</label>
+												</td>
+											</tr>
+											<tr>
+												<th>ラジオボタン</th>
+												<td>
+													<input type="radio" name="radio" id="radio1"><label for="radio1">radio1</label>
+													<input type="radio" name="radio" id="radio2"><label for="radio2">radio2</label>
+												</td>
+											</tr>
+											<tr>
+												<th>セレクト</th>
+												<td>
+													<div class="c-form__select">
+														<label><select> <option>1</option> <option>2</option> <option>3</option> <option>4</option> <option>5</option> </select></label>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<th>テキストエリア</th>
+												<td>
+													<textarea name="" id="" cols="30" rows="10"></textarea>
+												</td>
+											</tr>
+										</table>
+									</form>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+				<div class="c-grid__col c-grid__col--3">
+					<aside>
+						<h2 class="c-title c-title--primary">Menu<span class="c-title__sub-text">メニュー</span></h2>
+						<div class="c-card">
+							<ul class="c-menu">
+								<li class="c-menu__item">
+									<a href="#">
+										メニュー1
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+								<li class="c-menu__item">
+									<a href="#">
+										メニュー1
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+								<li class="c-menu__item">
+									<a href="#">
+										メニュー1
+										<i class="c-menu__item__icon fa fa-angle-right"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="u-ta-r">
+							<a href="#groupSuccesModal" class="c-btn c-btn-primary--flat js-modal-btn">Modal Open</a>
+						</div>
+						<div id="groupSuccesModal" class="js-modal c-modal">
+							<div class="c-modal__header">
+								モーダルタイトル
+							</div>
+							<div class="c-modal__content">
+								modal
+							</div>
+							<div class="c-modal__bottom">
+								<button class="c-btn c-btn-default--flat js-modal-close-btn">閉じる</button>
+							</div>
+							<i class="js-modal-close-btn c-modal__close fa fa-times-circle"></i>
+						</div>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer role="contentinfo">
+		フッター
+	</footer>
+
+	<script src="js/jquery-2.1.1.min.js"></script>
+	<script src="js/jquery.simpledrawer.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
